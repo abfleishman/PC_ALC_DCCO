89d9448cdd5626dfd6cef4258484298332ad53d8
abfleishman <abfleishman@gmail.com>
2017-04-28 09:19:43 -0700

    Sections

--- DCCO_Analysis.R
+++ DCCO_Analysis.R
@@ -1,24 +1,49 @@
+
+# Load librarys -----------------------------------------------------------
+
+
 library(tidyverse)
 library(readxl)
 library(lubridate)
 library(zoo)
+
+
+# Load data ---------------------------------------------------------------
+
+
 nests<-read_excel("Data/DCCO_Nest_Data_All_Years_04-27-17.xlsx",sheet=3)
 head(nests)
+# Renames and sort by date
 nests<-nests %>% rename(Count=`Active Nests`) %>% arrange(date)
 
+# Calculate days from 1 Sep
 nests$DOS=as.numeric((as.Date(nests$date)-ymd(paste(nests$Season,"09","01"))))
-head(nests)
-maxCount<-nests %>% group_by(Season) %>%top_n(n = 5,wt = Count) %>%
-  summarise(max=max(Count),mean=mean(Count),median=median(Count),sd=sd(Count),n=n())
-maxCount
 
-ggplot(maxCount,aes(Season,max,group=Season))+geom_bar(stat="identity",fill="dodgerblue")+theme_bw(base_size = 24)
+
+# Summarize by season using top 5 counts ----------------------------------
+
+Counts<-nests %>% group_by(Season) %>%top_n(n = 5,wt = Count) %>%
+  summarise(max=max(Count),mean=mean(Count),median=median(Count),sd=sd(Count),n=n())
+
+Counts
+
+# Summary figures ---------------------------------------------------------
+
+# Max count by year
+ggplot(Counts,aes(Season,max,group=Season))+geom_bar(stat="identity",fill="dodgerblue")+theme_bw(base_size = 24)
 ggsave("Plots/max_nest_top_5.jpg")
-ggplot(maxCount,aes(Season,median,group=Season))+geom_bar(stat="identity",fill="dodgerblue")+theme_bw(base_size = 24)
+
+# median count by year
+ggplot(Counts,aes(Season,median,group=Season))+geom_bar(stat="identity",fill="dodgerblue")+theme_bw(base_size = 24)
 ggsave("Plots/median_nest_top_5.jpg")
-ggplot(maxCount,aes(Season,mean,group=Season))+geom_bar(stat="identity",fill="dodgerblue")+
+
+# Mean count by year
+ggplot(Counts,aes(Season,mean,group=Season))+geom_bar(stat="identity",fill="dodgerblue")+
   geom_errorbar(aes(ymin=mean-sd,ymax=mean+sd))+theme_bw(base_size = 24)
 ggsave("Plots/mean_nest_top_5.jpg")
+
+
+# Seasonal figures --------------------------------------------------------
 
 ggplot(nests,aes(DOS,Count,group=Season,color=factor(Season)))+
   geom_path()+
@@ -45,8 +70,12 @@ ggplot(nests,aes(DOS,Count,group=Season,color=factor(Season)))+
   geom_point(data=top3Count,aes(DOS,Count,group=Season),color='red')
 ggsave("Plots/season_Count_with_top3.jpg",width = 5,height = 12,units = "in")
 
+
+# Rolling Mean figures ----------------------------------------------------
+
 roll<-nests %>% group_by(Season) %>% mutate(rollCount2=rollmean(Count,k=2,align = "right", fill = NA),
                                             rollCount3=rollmean(Count,k=3,align = "right", fill = NA))
+
 ggplot(roll,aes(DOS,rollCount2,group=Season,color=factor(Season)))+
   geom_path()+
   geom_point()+
